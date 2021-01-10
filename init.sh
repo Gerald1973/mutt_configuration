ee0b51a7eb91f9c88ec928aeec36064da8e3641f
Gerald Marechal <gerald1973@gmail.com>
2021-01-10 17:41:41 +0100

    Interactive config of mutt ok.

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+printf 'Replacment of the variables concerning the user name and the password in the mutt configuration file.\n'
+printf '=====================================================================================================\n'
+printf 'User name :\n'
+read userName
+printf 'Password :\n'
+read password
+printf 'Real name :\n'
+read realName
+sed "s/@password/$password/g; s/@userName/$userName/g; s/@realName/$realName/g" muttrc > muttrcgenerated
+if [ $? = 0 ]; then
+    printf 'Are you sure to override the .muttrc configuration file? (Y/N)'
+    read answer
+    if [ "${answer^^}" = "Y" ]; then
+        mv -i ./muttrcgenerated ~/.muttrc
+        if [ $? = 0 ]; then 
+            printf 'Operation successful.\n'
+        fi
+    fi
+fi
